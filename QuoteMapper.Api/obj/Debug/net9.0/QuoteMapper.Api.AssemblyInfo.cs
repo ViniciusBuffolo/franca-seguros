d6a5aedf888cec7862b18be5e46bbe6425a7e9d2
ViniciusBuffolo <vinicius.buffolo@gmail.com>
2026-04-15 18:37:28 -0300

    Remove docker-compose.yml and create Dockerfile

--- QuoteMapper.Api/obj/Debug/net9.0/QuoteMapper.Api.AssemblyInfo.cs
+++ QuoteMapper.Api/obj/Debug/net9.0/QuoteMapper.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuoteMapper.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70b6f5de952c8baacbd9aa7a1b41c835b988589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c754ecf53613607718bc17f3d976c69f981b83ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuoteMapper.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuoteMapper.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
